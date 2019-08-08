f203cecfcd7d8bca178eb0f3fcb60fc9811aa46f
Zoltaroth <nandor@ntsj.com>
2019-08-07 21:59:59 -0700

    spacing fix

--- create_docker.sh
+++ create_docker.sh
@@ -2,7 +2,7 @@ docker kill unicorn-transcoder 2>/dev/null
 docker rm unicorn-transcoder 2>/dev/null
 
 docker create --name unicorn-transcoder \
-			  --restart unless-stopped \
+              --restart unless-stopped \
               -p 3000:3000 \
               -v $(pwd)/config.js:/opt/UnicornTranscoder/config.js \
                 nandordocker/plex-unicorn-transcoder:latest
